e2cd4c73fdcfd9dcc3f2fd2fcbdb7610e47e5401
WaterStop <3045672234@qq.com>
2023-04-20 09:56:05 +0800

    ch1 success

--- os/target/riscv64gc-unknown-none-elf/release/deps/os-cdf1cd26066f26e5.d
+++ os/target/riscv64gc-unknown-none-elf/release/deps/os-cdf1cd26066f26e5.d
@@ -1,6 +1,6 @@
-/home/ws/2023s-rcore-WaterStop/rCore-Tutorial-Code-2023S/os/target/riscv64gc-unknown-none-elf/release/deps/os-cdf1cd26066f26e5: src/main.rs src/console.rs src/lang_items.rs src/logging.rs src/sbi.rs src/boards/qemu.rs src/entry.asm
+/home/ws/2023s-rcore-WaterStop/os/target/riscv64gc-unknown-none-elf/release/deps/os-cdf1cd26066f26e5: src/main.rs src/console.rs src/lang_items.rs src/logging.rs src/sbi.rs src/boards/qemu.rs src/entry.asm
 
-/home/ws/2023s-rcore-WaterStop/rCore-Tutorial-Code-2023S/os/target/riscv64gc-unknown-none-elf/release/deps/os-cdf1cd26066f26e5.d: src/main.rs src/console.rs src/lang_items.rs src/logging.rs src/sbi.rs src/boards/qemu.rs src/entry.asm
+/home/ws/2023s-rcore-WaterStop/os/target/riscv64gc-unknown-none-elf/release/deps/os-cdf1cd26066f26e5.d: src/main.rs src/console.rs src/lang_items.rs src/logging.rs src/sbi.rs src/boards/qemu.rs src/entry.asm
 
 src/main.rs:
 src/console.rs:
